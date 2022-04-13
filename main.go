607be1dee290370a4b77cb76917da9478af746ee
Matheus <germano.prog@gmail.com>
2022-04-13 17:57:04 -0300

    feat: terminal call structuring

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	// First address
+	fmt.Println("Enter first address:")
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	firstAddress := strings.TrimSuffix(text, "\r\n")
+
+	// Second address
+	fmt.Println("Enter second address:")
+	text, err = reader.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	secondAddress := strings.TrimSuffix(text, "\r\n")
+
+	geo1, err := GetCoordinatesFromAddress(firstAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	geo2, err := GetCoordinatesFromAddress(secondAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(geo1.Results) == 0 || len(geo2.Results) == 0 {
+		panic(errors.New("One of the address (or both of them) was not found"))
+	}
+
+	mi, km := EuclidianDistance(geo1.Results[0].Geometry.Location, geo2.Results[0].Geometry.Location)
+
+	fmt.Printf("\n\nFirst address: %s\nFirst Address Geometry: %v / %v\nSecond address: %s\nSecond Address Geometry: %v / %v\n\nDistance in Miles: %v\nDistance in KM: %v",
+		firstAddress, geo1.Results[0].Geometry.Location.Latitude, geo1.Results[0].Geometry.Location.Longitude,
+		secondAddress, geo2.Results[0].Geometry.Location.Latitude, geo2.Results[0].Geometry.Location.Longitude,
+		mi, km)
+}

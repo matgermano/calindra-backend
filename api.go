785d96cc1c02ed80b85bd2103de6d28f03d3d7e6
Matheus <germano.prog@gmail.com>
2022-04-13 17:53:57 -0300

    feat: calling the api

---
+++ api.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+)
+
+const (
+	baseEndpoint = "https://maps.googleapis.com/maps/api/geocode/json?address="
+)
+
+func GetCoordinatesFromAddress(address string) (*Geo, error) {
+	uri := url.QueryEscape(address)
+	endpoint := baseEndpoint + uri
+	finalEndpoint := endpoint + "&key=AIzaSyAutJabWt9acnFnpqw6CYpsz25AE9ttgWw"
+
+	x, err := http.Get(finalEndpoint)
+	if err != nil {
+		return nil, err
+	}
+	defer x.Body.Close()
+
+	var geo Geo
+	err = json.NewDecoder(x.Body).Decode(&geo)
+	if err != nil {
+		return nil, err
+	}
+
+	return &geo, nil
+}
